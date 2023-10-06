6825d4894651f16ae640c3dc3145120bf57f5ac2
Sephley <hurley.joseph@protonmail.ch>
2023-10-06 16:14:51 +0200

    Tappy up to step 30

--- Tappy/plane_cb/plane_cb.gd
+++ Tappy/plane_cb/plane_cb.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 
 
+@onready var animation_player = $AnimationPlayer
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+
 const GRAVITY: float = 1900.0
 const POWER: float = -400.0
 
@@ -15,7 +19,20 @@ func _physics_process(delta):
 
 	velocity.y += GRAVITY * delta
 	
-	if Input.is_action_just_pressed("fly") == true:
-		velocity.y = POWER
+	fly()
 	
 	move_and_slide()
+	
+	if is_on_floor() == true:
+		die()
+
+func fly() -> void:
+	
+	if Input.is_action_just_pressed("fly") == true:
+		velocity.y = POWER
+		animation_player.play("fly")
+
+func die() -> void:
+	animated_sprite_2d.stop()
+	set_physics_process(false)
+	
